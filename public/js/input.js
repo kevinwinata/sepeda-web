f9551f4b4daed15de893657835e4e2ee2d7c3121
Kevin Winata <kevinwinata@gmail.com>
2015-08-12 17:52:34 +0700

    Input page dynamic form based on selected class

--- public/js/input.js
+++ public/js/input.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 	$('select').material_select();
 	$('.modal-trigger').leanModal();
+	console.log(local_data);
+
+	$("#class-select").change(function(){
+		var idx = parseInt($("#class-select").val());
+		$("#form-class").html(function(){
+			var str = "";
+			for (var i = 0; i < local_data[idx].properties.length; i++) {
+				str +=  '<div class="input-field col s12">' + 
+						'<input id="input'+i+'" type="tel">' + 
+					 	'<label for="input'+i+'">'+local_data[idx].properties[i]+'</label>' + 
+					 	'</div>';
+			}
+			return str;
+		});
+	});
 });
--- routes/input.js
+++ routes/input.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 router.get('/', function(req, res, next) {
 	var db = req.db;
 	db.class.list()
 	.then(function (result) {
-		console.log('There are ' + result.length + ' classes in the db:', result);
-		res.render('input', {data:result});
+		//console.log(result);
+		var temp = [];
+		for (var i = 0; i < result.length; i++) {
+			temp[i] = {};
+			temp[i].name = result[i].name;
+			temp[i].properties = [];
+			for (var j = 0; j < result[i].properties.length; j++) {
+				temp[i].properties[j] = result[i].properties[j].name;
+			}
+		}
+		res.render('input', {data: temp});
 	});
 });
 
